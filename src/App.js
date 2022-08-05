2e7b6c60bce6c528a6c50cdafac2119c1737e879
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-08-05 13:57:57 -0300

    adjusting global styles

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import React from "react";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 
+import { GlobalStyle } from "./Components/globalStyles";
+
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Cabecalho />
       <Container />
     </>
