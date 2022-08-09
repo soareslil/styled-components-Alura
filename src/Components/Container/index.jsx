ca308228064fc0c22c0716301be0be910c29cdfb
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-08-09 11:11:34 -0300

    refatorando container component

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 
 import Titulo from "../Titulo/index";
 import Conta from "../Conta";
 
+const ContainerDiv = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`;
+
+const Conteudo = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
 const Container = () => {
   return (
-    <div className="container">
+    <ContainerDiv>
       <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
+      <Conteudo>
         <Conta />
-      </section>
-    </div>
+      </Conteudo>
+    </ContainerDiv>
   );
 };
 
