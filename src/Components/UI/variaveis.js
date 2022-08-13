9c8b6a0603e30af4cd52d953fce43ae76cbc0a19
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-08-13 15:42:18 -0300

    criando novas variaves de cores

--- src/Components/UI/variaveis.js
+++ src/Components/UI/variaveis.js
@@ -1,3 +1,10 @@
 export const corPrimaria = 'lightblue';
-export const fundoClaro = 'white';
+
+export const fundoClaro = '#f1f1f1';
+export const conteudoClaro = 'white';
 export const textoFundoClaro = "gray";
+
+
+export const fundoEscuro = "#363537";
+export const conteudoEscuro = "#5c5b5e";
+export const textoFundoEscuro = "FAFAFA";
